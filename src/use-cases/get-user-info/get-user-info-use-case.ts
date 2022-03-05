57096ff721b806c3d120fc63b17949961d571f07
Pedro Tashima <pedrotashima42@gmail.com>
2022-03-05 17:27:22 -0300

    fix: subscriber id variable

--- src/use-cases/get-user-info/get-user-info-use-case.ts
+++ src/use-cases/get-user-info/get-user-info-use-case.ts
@@ -1,23 +1,18 @@
 import {GetUserInfoRequestDTO} from "./get-user-info-request-DTO"
-
 import {GetUserInfoResponseDTO} from "./get-user-info-response-DTO"
-//import {IUserRepository} from "../../repositories/IUserRepository"
 import {ITokenRepository} from "../../repositories/ITokenRepository"
 
 export class GetUserInfoUseCase {
-  constructor(
-    //private userRepository: IUserRepository,
-    private tokenRepository: ITokenRepository,
-  ) {}
+  constructor(private tokenRepository: ITokenRepository) {}
 
   async execute(data: GetUserInfoRequestDTO): Promise<GetUserInfoResponseDTO> {
     const {token} = data
     const tokenFound = await this.tokenRepository.getByAccessToken(token)
     if (!tokenFound) throw {code: "UC-GI-001", message: "Token not found"}
-    const {user: {country, id}} = tokenFound
+    const {user: {country, subscriberId}} = tokenFound
 
     const userInfo: GetUserInfoResponseDTO = {
-      subscriber_id: id,
+      subscriber_id: subscriberId,
       country_code: country,
     }
     return userInfo
